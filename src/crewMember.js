56887d36ae52e74656c85b8276418c14bc90b8de
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-12 23:29:29 +0000

    Automatically backed up by Learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,3 +1,4 @@
+
 class CrewMember {
   constructor(position) {
     this.position = position;
@@ -9,16 +10,16 @@ class CrewMember {
       ? (this.currentShip.warpDrive = 'engaged')
       : 'had no effect';
   }
-  
+
   setsInvisibility() {
-    return this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig'
+    return this.position === 'Defender' && this.currentShip !== 'Looking for a Rig'
       ? (this.currentShip.cloaked = true)
       : 'had no effect';
   }
-  
+
   chargePhasers() {
     return this.position === 'Gunner' && this.currentShip !== 'Looking for a Rig'
       ? (this.currentShip.phasersCharge = 'charged')
-      : 'had no effect';      
+      : 'had no effect';
   }
 }
